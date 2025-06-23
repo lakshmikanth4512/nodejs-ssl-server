257bbb3ad1f4f462471b23ca881dd6d2a9e50cc3
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 00:01:37 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "===== START SCRIPT STARTED ====="
-
-IMAGE_URI=845091770108.dkr.ecr.ap-south-1.amazonaws.com/nodejs-ssl-server:latest
-
-# Login to ECR and pull the image
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 845091770108.dkr.ecr.ap-south-1.amazonaws.com
+echo "Starting container..."
+IMAGE_URI="845091770108.dkr.ecr.ap-south-1.amazonaws.com/nodejs-ssl-server:latest"
 docker pull $IMAGE_URI
-
-# Run the container
 docker run -d -p 3000:3000 --name nodejs-app $IMAGE_URI
-
-echo "===== START SCRIPT ENDED ====="
