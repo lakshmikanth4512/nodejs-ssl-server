afcc1d39cb7225d7957c2ca26dcb685a4c64a1e4
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 00:02:47 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Starting container..."
+
 IMAGE_URI="845091770108.dkr.ecr.ap-south-1.amazonaws.com/nodejs-ssl-server:latest"
+
 docker pull $IMAGE_URI
 docker run -d -p 3000:3000 --name nodejs-app $IMAGE_URI
+
+echo "Container started."
