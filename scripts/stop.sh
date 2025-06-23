8c7918c84848a3d7556152eaccb0bc4c37bddfe9
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 00:07:16 +0530

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "===== STOP SCRIPT STARTED ====="
 
-# Stop and remove old container
-if docker ps -a --format '{{.Names}}' | grep -Eq "^nodejs-app$"; then
-  echo "Stopping existing container..."
+# Stop and remove old container if it exists
+if docker ps -a --format '{{.Names}}' | grep -wq "nodejs-app"; then
+  echo "Stopping existing container: nodejs-app"
   docker stop nodejs-app
   docker rm nodejs-app
 else
-  echo "No running container found with name nodejs-app"
+  echo "No container found with name nodejs-app"
 fi
 
 echo "===== STOP SCRIPT ENDED ====="
